643f4152e20d9e7558c0b0287a719cc4ab42d689
PC <PC@DESKTOP-8MN65CJ>
2025-10-16 16:50:32 +0300

    Добавьте файлы проекта.

---
+++ Lab08.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdio.h>
+
+#include <locale.h>
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int n;
+	double x;
+	double res = 0,0;
+	printf("Введите натуральное n: ");
+	scanf("%d", &n);
+
+	printf("Введите действительное x: ");
+	scanf("%lf", &x);
+	for (int i = 1; i <= n; i++)
+	{
+		double power = pow(x, i);
+		double func = cos(power);
+		res += func;
+
+	}
+	printf("\nИтоговая сумма = %.6f\n", res);
+}
